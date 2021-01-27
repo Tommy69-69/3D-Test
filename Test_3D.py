1d1265f681e4851b4520f498fcaee38239d564ae
Tommy69-69 <76016177+Tommy69-69@users.noreply.github.com>
2021-01-26 22:02:38 -0500

    Create Test_3D.py

---
+++ Test_3D.py
@@ -0,0 +1,18 @@
+"""
+@author Francisco Grijalva
+@date January 26th, 2021
+Rotate a circle 3D
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+#import functions tools3D
+import tools3D as Tools
+
+plt.axis([0,150,100,0])
+plt.axis()
+plt.grid(False)
+#----Code lines uwu
+
+
+plt.show()
